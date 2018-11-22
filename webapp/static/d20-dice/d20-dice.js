69f67bbd9f5244c9f951e167482fb1f61d75e9a5
Stefan Safranek <stefan@stefan>
2018-11-21 17:08:05 -0800

    dice roller ui

--- webapp/static/d20-dice/d20-dice.js
+++ webapp/static/d20-dice/d20-dice.js
@@ -10,6 +10,9 @@ var Random = {
 var Dice = {
 
     roll: function(ntimes, nsides, modifier) {
+        if (!modifier) {
+            modifier = 0;
+        }
         var rolls = [];
         for (var i=0; i<ntimes; i++) {
             rolls.push(
@@ -45,6 +48,77 @@ var Dice = {
 
     d20: function(ntimes, modifier) {
         return this.roll(ntimes, 20, modifier);
-    }
+    },
 
+    d100: function(ntimes, modifier) {
+        return this.roll(ntimes, 100, modifier);
+    }
 }
+
+
+var DiceUi = function() {
+    var self = this
+
+    var rollTimes = $("<input>", {type:"number", min:1, step:1})
+        .addClass('form-control form-control-sm')
+        .val(1);
+
+    var diceTypeSelector = $("<select>")
+        .addClass("form-control form-control-sm")
+        .append(
+            $('<option>', {value: 'd2'}).append('d2'),
+            $('<option>', {value: 'd4'}).append('d4'),
+            $('<option>', {value: 'd6'}).append('d6'),
+            $('<option>', {value: 'd8'}).append('d8'),
+            $('<option>', {value: 'd10'}).append('d10'),
+            $('<option>', {value: 'd12'}).append('d12'),
+            $('<option>', {value: 'd20'}).append('d20'),
+            $('<option>', {value: 'd100'}).append('d100')
+        );
+
+    var rollModifier = $("<input>", {type:"number", min:0, step:1})
+        .addClass('form-control form-control-sm')
+        .val(0);
+
+    var resultsContainer = $("<div>");
+
+    var rollDiceButton = $("<button>")
+        .addClass("btn btn-primary btn-sm")
+        .append("Roll")
+        .on("click", function() {
+            resultsContainer.empty();
+            var dtype = diceTypeSelector.val();
+            var rolls = Dice[dtype](
+                parseInt(rollTimes.val()),
+                parseInt(rollModifier.val())
+            );
+            for (var i=0; i<rolls.length; i++) {
+                resultsContainer.append(
+                    $('<div>').append(rolls[i])
+                );
+            }
+        });
+
+    this.content = $("<div>")
+        .append(
+            $("<div>")
+                .addClass("form-inline")
+                .append(
+                    rollTimes,
+                    diceTypeSelector,
+                    rollModifier,
+                    rollDiceButton
+                ),
+            resultsContainer
+        );
+
+    this.container = DM.ui.makeDMToolUi({
+        "name": "Dice",
+        "content": this.content
+    });
+
+    $("body").append(this.container);
+}
+
+
+DM.registerTool("Dice", DiceUi);
